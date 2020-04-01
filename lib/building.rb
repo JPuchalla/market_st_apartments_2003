4665baeabb1181f8b3d6012aa958276d3bbb20d3
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-04-01 10:47:25 -0600

    Paired Work

--- lib/building.rb
+++ lib/building.rb
@@ -38,9 +38,39 @@ class Building
   end
 
   def renter_with_highest_rent
-   highest_rented = rented_units.map do
-     |unit| unit.monthly_rent
-     #didn't pass obiously
+    highest_unit = rented_units.max_by do |unit|
+      unit.monthly_rent
+    end
+    highest_unit.renter
+  end
+
+  def units_by_number_of_bedrooms
+   bedrooms = {}
+   @units.each do |unit|
+     if bedrooms.keys.include?(unit.bedrooms)
+       bedrooms[unit.bedrooms] << unit.number
+       # require "pry"; binding.pry
+     else
+     bedrooms[unit.bedrooms] = [unit.number]
+     # require "pry"; binding.pry
+     end
    end
+   bedrooms
+  end
+
+  def annual_breakdown
+  breakdown = {}
+  rented_units.each do |unit|
+    # require "pry"; binding.pry
+    breakdown[unit.renter.name] = unit.monthly_rent * 12
+    end
+  breakdown
+  end
+
+  def rooms_by_renter
+    renter.group_by do |rentee|
+      # require "pry"; binding.pry
+
+    end
   end
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -54,18 +54,56 @@ class RenterTest < Minitest::Test
     @building.add_unit(@unit2)
     @building.add_unit(@unit3)
 
+
     assert_equal [], @building.rented_units
     @unit2.add_renter(@renter3)
     assert_equal [@unit2], @building.rented_units
+    @unit3.add_renter(@renter1)
+    assert_equal [@unit2, @unit3], @building.rented_units
   end
 
-  def test_if_it_can_find_renter_with_highest_rent
+  def test_returns_renter_with_highest_rent
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+    @unit2.add_renter(@renter1)
+    assert_equal @renter1, @building.renter_with_highest_rent
+    @unit1.add_renter(@renter2)
+    assert_equal @renter2, @building.renter_with_highest_rent
     @unit1.add_renter(@renter3)
-    assert_equal [@renter3], @building.renter_with_highest_rent
-    @unit3.add_renter(@renter4)
-    assert_equal [@renter3], @building.renter_with_highest_rent
-    @unit3.add_renter(@renter5)
+    assert_equal @renter3, @building.renter_with_highest_rent
+  end
+
+  def test_if_it_can_calc_units_by_num_bed_rooms
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
     @building.add_unit(@unit4)
-    assert_equal [@renter3], @building.renter_with_highest_rent
+    insert =  {
+       3 => ["D4" ],
+       2 => ["B2", "C3"],
+       1 => ["A1"]
+     }
+     assert_equal insert, @building.units_by_number_of_bedrooms
+  end
+
+  def test_returns_annual_breakdown
+   @building.add_unit(@unit1)
+   @building.add_unit(@unit2)
+   @building.add_unit(@unit3)
+   @building.add_unit(@unit4)
+   @unit2.add_renter(@renter3)
+   assert_equal ({"Spencer" => 11988}), @building.annual_breakdown
+   @unit1.add_renter(@renter4)
+   assert_equal ({"Jessie" => 14400, "Spencer" => 11988}), @building.annual_breakdown
+  end
+
+  def test_can_retrieve_rooms_by_renter
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @unit1.add_renter(@renter3)
+    @unit2.add_renter(@renter4)
+
+    assert_equal
   end
 end
